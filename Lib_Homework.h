422b9a596a5c64c8cf33bed4cb2fa90fdae34e09
sunset <jaredlares5@gmail.com>
2023-03-28 23:02:51 -0600

    Funciones PoN y SyS 28/03/2023

--- Lib_Homework.h
+++ Lib_Homework.h
@@ -10,19 +10,49 @@ void PoN()
 	do
 	{
 		cout << "ingrese que numero dessea evaluar" << endl;
-			
-	} while (true);
+		cin >> n;
+		if (n==0)
+		{
+			cout << "intente de nuevo" << endl;
+		}
+		else if (n>0)
+		{
+			cout << "El numero es positivo" << endl;
+		}
+		else 
+		{
+			cout << "El numero es negativo" << endl;
+		}
+	} while (salir);
+}
+void SyS() 
+{
+	int n = 0, suma = 0,k;
+	cout << "ingrese un numero de o mas cifras" << endl;
+	cin >> n;
+	k = n;
+	while (n>0)
+	{
+		suma += n % 10;
+		n = n / 10;
+	}
+	cout << " La suma del numero " << k << " es: " << suma << endl;
+	system("pause");
 }
 void Tarea_1()
 {
 	int Menu_tarea1 = 0;
 	cout << "	----------A que ejercicio dessea ingresar----------" << endl;
-	cout << "1) Evaluar si un numero es mayor o menor" << endl;
+	cout << "1) Evaluar si un numero es Positivo o Negativo" << endl;
+	cout << "2) " << endl;
 	cin >> Menu_tarea1;
 	switch (Menu_tarea1)
 	{
 	case 1:
 		PoN();
+		break;
+	case 2:
+		SyS();
 		break;
 	default:
 		break;
